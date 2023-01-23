6a8bc9bb8a417bd323ee94eec46b45afc32a457b
Fonji Terence <terencehilln@gmail.com>
2023-01-23 21:53:45 +0100

    Initialisation projet

---
+++ efactures/src/main/java/com/digitalyouthservices/efactures/EfacturesApplication.java
@@ -0,0 +1,13 @@
+package com.digitalyouthservices.efactures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfacturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfacturesApplication.class, args);
+	}
+
+}
