699cb0bdc3b04f91b14a03b13afa78e767e67a87
Fonji Terence <terencehilln@gmail.com>
2023-01-24 01:58:48 +0100

    Update ArticleRepository

--- efactures/src/main/java/com/digitalyouthservices/efactures/repository/ArticleRepository.java
+++ efactures/src/main/java/com/digitalyouthservices/efactures/repository/ArticleRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
-    List<ClientEntity> findByEmail(String email);
+    
 }
